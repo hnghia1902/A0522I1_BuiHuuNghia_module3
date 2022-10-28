6fff678aa1b232f628cee4fdb76303052500b3eb
ád ád <you@example.com>
2022-10-28 20:08:57 +0700

    ,

---
+++ ss_03/QuanLySinhVien.sql
@@ -0,0 +1,8 @@
+use ss_03;
+SELECT * FROM student WHERE StudentName LIKE 'h%';
+SELECT * FROM class WHERE month(startDate) = 12;
+SELECT * FROM `subject` WHERE Credit BETWEEN 3 AND 5;
+SELECT * FROM student;
+UPDATE student SET classId = 2 WHERE StudentName = 'Hung';
+SELECT student.StudentName, subject.SubName, mark.Mark FROM student, mark, subject
+ORDER BY mark.Mark DESC, student.StudentName;
